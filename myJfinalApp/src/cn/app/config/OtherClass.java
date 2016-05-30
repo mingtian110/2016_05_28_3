a80a988399c47764956b54887c30455b07529cec
mingruishen <1109610189@qq.com>
2016-05-30 13:40:38 +0800

    被版本

--- myJfinalApp/src/cn/app/config/OtherClass.java
+++ myJfinalApp/src/cn/app/config/OtherClass.java
@@ -3,6 +3,7 @@ package cn.app.config;
 public class OtherClass {
 
 	public static void main(String[] args) {
+		System.out.println("hello again");
 		System.out.println("hello");
 		System.out.println("hello two");
 	}
