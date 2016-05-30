bc326d15b93584696130ae6c8c16f80771913f19
mingruishen <1109610189@qq.com>
2016-05-30 14:00:16 +0800

    rebase 版本

--- myJfinalApp/src/cn/app/config/OtherClass.java
+++ myJfinalApp/src/cn/app/config/OtherClass.java
@@ -3,6 +3,7 @@ package cn.app.config;
 public class OtherClass {
 
 	public static void main(String[] args) {
+		System.out.println("hello again too rebase");
 		System.out.println("hello again too");
 		System.out.println("hello again");
 		System.out.println("hello");
